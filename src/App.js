20d62b1773d7de5aa0f53057077791f2c041d8a6
李雪强 <786665858@qq.com>
2020-04-21 15:23:21 +0800

    app.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          我的第一个React APP<code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
